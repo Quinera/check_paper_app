2cab9fc6a487c6b1057a313864fda3f1f0bbdce0
Quinera <kaoriinoue94@gmail.com>
2024-03-23 20:17:58 +0900

    update.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_file, render_template,url_for
+from flask import Flask, request, send_file, render_template, url_for
 import regex as re
 import os
 
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # パターンとその名称を辞書で定義
 patterns = {
     "1文の長さが長すぎる": r".{100,}",
-    #"pp.とページ番号の間に半角スペースがない": r"pp\.(?!\s\d+--\d+)",
+    # "pp.とページ番号の間に半角スペースがない": r"pp\.(?!\s\d+--\d+)",
     "「です」「ます」調": r"です|ます|でしょう|でした|ました|ません",
     "体言止め": r"[\u4E00-\u9FFF]。",
     "半角カタカナ": r"[\uFF61-\uFF9F]+",
@@ -23,116 +23,123 @@ patterns = {
     "ダブルクオーテーションの誤り": r'"',
     "カッコとカッコの外側の単語の間に半角スペースがない": r"[\p{Han}\p{Hiragana}\p{Katakana}a-zA-Z]\(|\)[\p{Han}\p{Hiragana}\p{Katakana}a-zA-Z]",
     "半角カンマと英単語の間に半角スペースがない": r",[a-zA-Z0-9!-/:-@[-`{-~]+",
-    "ソースファイルで未改行": r'(。[^。]*。)',
+    "ソースファイルで未改行": r"(。[^。]*。)",
     "句点の後に参考文献": r"。\\cite|\. \\cite",
     "半角文字と参考文献の間に半角スペースがない": r"[\x21-\x7e]\\cite",
     "4桁以上の数字にカンマがない": r"\d{4,}",
 }
 
+
 # ファイルをアップロードし、処理するエンドポイント
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def upload_file():
-    if request.method == 'POST':
+    if request.method == "POST":
         # ファイルがリクエストに含まれているか確認
-        if 'file' not in request.files:
-            return 'ファイルがありません', 400
-        file = request.files['file']
-        if file.filename == '':
-            return 'ファイルが選択されていません', 400
+        if "file" not in request.files:
+            return "ファイルがありません", 400
+        file = request.files["file"]
+        if file.filename == "":
+            return "ファイルが選択されていません", 400
         if file:
             # アップロードされたファイルを一時ファイルとして保存
-            input_file_path = os.path.join('tmp', f'input_{file.filename}')
+            input_file_path = os.path.join("tmp", f"input_{file.filename}")
             file.save(input_file_path)
             # 出力ファイルの名前を設定
             output_file_name = f"result_{file.filename}"
-            output_file_path = os.path.join('tmp', output_file_name)
+            output_file_path = os.path.join("tmp", output_file_name)
             results = []
             # ファイルを処理
-            process_file(input_file_path, output_file_path,results)
+            process_file(input_file_path, output_file_path, results)
             # 処理後のファイルのダウンロードリンクを提供するページを表示
-            download_url = url_for('download_file', filename=output_file_name)
-            cleanup_url = url_for('cleanup', filename=file.filename)
-            return render_template('download.html', download_url=download_url, cleanup_url=cleanup_url, results=results)
+            download_url = url_for("download_file", filename=output_file_name)
+            cleanup_url = url_for("cleanup", filename=file.filename)
+            return render_template(
+                "download.html",
+                download_url=download_url,
+                cleanup_url=cleanup_url,
+                results=results,
+            )
 
     # GETリクエストの場合、ファイルアップロードフォームを表示（変更点）
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/download/<filename>')
+
+@app.route("/download/<filename>")
 def download_file(filename):
-    path = os.path.join('tmp', filename)
+    path = os.path.join("tmp", filename)
     return send_file(path, as_attachment=True)
 
-@app.route('/cleanup/<filename>', methods=['POST'])
+
+@app.route("/cleanup/<filename>", methods=["POST"])
 def cleanup(filename):
     try:
-        os.remove(os.path.join('tmp', f"result_{filename}"))
-        os.remove(os.path.join('tmp', f"input_{filename}"))
-        return 'File deleted successfully', 200
+        os.remove(os.path.join("tmp", f"result_{filename}"))
+        os.remove(os.path.join("tmp", f"input_{filename}"))
+        return "File deleted successfully", 200
     except Exception as e:
         return str(e), 500
 
-def output_result(output_file,line,line_number,name,results,search):
+
+def output_result(output_file, line, line_number, name, results, search):
     result = [f"{line_number}", f"{name}", f"{line.strip()}"]
     print_result = f"{result[0]} : {result[1]} - {result[2]}\n"
-    #print(print_result, end='')  # コンソールに表示
     output_file.write(print_result)  # ファイルに保存
-    #output_file.write("\n")  # ファイルに保存
     results.append(result)
 
-def find_and_save_patterns(file_path, output_file_path, patterns,results):
-    """
-    テキストファイルから指定された正規表現パターンにマッチする行を探し、
-    そのパターンの名称とマッチした行を別のファイルに保存する。
 
-    :param file_path: テキストファイルのパス
-    :param output_file_path: 結果を保存するファイルのパス
-    :param patterns: 正規表現パターンとその名称を含む辞書
-    """
+def find_and_save_patterns(file_path, output_file_path, patterns, results):
     bibflag = False
     cflag = False
-
-    with open(file_path, 'r', encoding='utf-8') as file, \
-        open(output_file_path, 'w', encoding='utf-8') as output_file:
+    with open(file_path, "r", encoding="utf-8") as file, open(
+        output_file_path, "w", encoding="utf-8"
+    ) as output_file:
         output_file.write("行数, 検出された問題, 文章\n")
         cflag = False
         for line_number, line in enumerate(file, start=1):
             line = line.strip()
-            if len(line) == 0: continue
-            if line.startswith('%') or line.startswith('\\') or "助成" in line:
-                if line.startswith('\\bibitem'): bibflag = True
+            if len(line) == 0:
                 continue
-
+            if line.startswith("%") or line.startswith("\\") or "助成" in line:
+                if line.startswith("\\bibitem"):
+                    bibflag = True
+                continue
             # cflagがTrueであれば、前の行と現在の行を結合
             if cflag:
                 line = previous_line + line
                 line_number = previous_number
-                cflag = False  # cflagをリセット
-                #print(f"{line}\n")
-
+                cflag = False
             # 文が句点で終わらない場合に、次の行と結合するための準備
-            if not line.endswith('。') and not line.endswith('．'):
+            if not line.endswith("。") and not line.endswith("．"):
                 previous_line = line
                 previous_number = line_number
                 cflag = True
                 continue
             else:
                 # 文が句点で終わっている場合は、cflagをFalseにして、次の行で結合しないようにする
-                if cflag: cflag = False
+                if cflag:
+                    cflag = False
             for name, pattern in patterns.items():
-                if name != "半角文字と参考文献の間に半角スペースがない" and name != "句点の後に参考文献":
-                    clean_line = re.sub(r'\\\w+\{[^}]*\}', '', line)
+                if (
+                    name != "半角文字と参考文献の間に半角スペースがない"
+                    and name != "句点の後に参考文献"
+                ):
+                    clean_line = re.sub(r"\\\w+\{[^}]*\}", "", line)
                 else:
                     clean_line = line
                 search = re.search(pattern, clean_line)
                 if search:
-                    if name == "4桁以上の数字にカンマがない" and bibflag: continue
-                    output_result(output_file,line,line_number,name,results,search)
-            if bibflag and line.endswith("."): bibflag = False
+                    if name == "4桁以上の数字にカンマがない" and bibflag:
+                        continue
+                    output_result(output_file, line, line_number, name, results, search)
+            if bibflag and line.endswith("."):
+                bibflag = False
 
-def process_file(input_file_path, output_file_path,results):
+
+def process_file(input_file_path, output_file_path, results):
     # 関数を実行
     find_and_save_patterns(input_file_path, output_file_path, patterns, results)
     pass
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
